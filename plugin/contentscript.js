633a14126c6165b7320606b0278e92ae3be967d9
Stef Chäser <info.stef.kaeser@gmail.com>
2019-10-15 15:53:29 +0200

    Add 3DS V2 cards

--- plugin/contentscript.js
+++ plugin/contentscript.js
@@ -1,4 +1,4 @@
-var initPluginScript = function() {
+﻿var initPluginScript = function() {
   console.log("initPluginScript for " + document.URL);
 
   var cvc = document.getElementById("payment-cardholdername-container");
@@ -14,63 +14,123 @@ var initPluginScript = function() {
   brand = brand == "eurocard" ? "mastercard" : brand;
 
   var testCreditCards = [
+      { 
+		type: "visa", 
+		number: "4111113333333333", 
+		cvc: "123", 
+		valid: false,
+		title : 'invalid',
+	  desc : 'invalid credit card, payment will fail'
+	},
     {
       type: "visa",
       number: "4000000000000002",
       cvc: "123",
       valid: true,
-      has3dSecure: true
+	  title : '/w 3DS V1',
+	  desc : 'valid card with 3D secure'
     },
     {
       type: "visa",
       number: "4111111111111111",
       cvc: "123",
       valid: true,
-      has3dSecure: false
+	  title : '/wo 3DS V1',
+	  desc : 'valid card without 3D secure'
     },
-    { type: "visa", number: "4111113333333333", cvc: "123", valid: false },
+	{ 
+		type: "visa", 
+		number: "4186455175836497", 
+		cvc: "123", 
+      valid: true,
+	  title : '/wo 3DS V2',
+	  desc : 'valid card with 3D secure V2 frictionless, no 3DS screen'
+	},
+	{ 
+		type: "visa", 
+		number: "4874970686672022", 
+		cvc: "123", 
+      valid: true,
+	  title : '/w 3DS V2',
+	  desc : 'valid card with 3D secure V2 with ACS Challenge Simulator'
+	},
     {
       type: "mastercard",
       number: "5300000000000006",
       cvc: "123",
       valid: true,
-      has3dSecure: true
+	  title : '/w 3DS V1',
+	  desc : 'valid card with 3D secure'
     },
     {
       type: "mastercard",
       number: "5399999999999999",
       cvc: "123",
       valid: true,
-      has3dSecure: false
+	  title : '/wo 3DS V1',
+	  desc : 'valid card without 3D secure'
     },
+	{ 
+		type: "mastercard", 
+		number: "5137009801943438", 
+		cvc: "123", 
+      valid: true,
+	  title : '/wo 3DS V2',
+	  desc : 'valid card with 3D secure V2 frictionless, no 3DS screen'
+	},
+	{ 
+		type: "mastercard", 
+		number: "5130257474533310", 
+		cvc: "123", 
+      valid: true,
+	  title : '/w 3DS V2',
+	  desc : 'valid card with 3D secure V2 with ACS Challenge Simulator'
+	},
     {
       type: "american express",
       number: "371449635311004",
       cvc: "1234",
       valid: true,
-      has3dSecure: true
+	  title : '/wo 3DS V1',
+	  desc : 'valid card without 3D secure'
     },
     {
       type: "american express",
       number: "374111111111111",
       cvc: "1234",
       valid: true,
-      has3dSecure: false
-    }
+	  title : '/wo 3DS V1',
+	  desc : 'valid card without 3D secure'
+    },
+	{ 
+		type: "american express", 
+		number: "375418081197346", 
+		cvc: "123", 
+      valid: true,
+	  title : '/wo 3DS V2',
+	  desc : 'valid card with 3D secure V2 frictionless, no 3DS screen'
+	},
+	{ 
+		type: "american express", 
+		number: "379764422997381", 
+		cvc: "123", 
+      valid: true,
+	  title : '/w 3DS V2',
+	  desc : 'valid card with 3D secure V2 with ACS Challenge Simulator'
+	}
   ];
 
-  function createButton(container, name, color, card) {
-    if (card == null || card.length <= 0) {
-      return;
-    }
-    card = card[0];
-    var newButton = document.createElement("div");
-    newButton.innerHTML = name;
+  function createButton(container, card) {
+   
+   var color = card.valid ? '#6ecc6e' : '#eb5b56';
+    var newButton = document.createElement("input");
+    newButton.value = card.title;
+	newButton.type = 'button';
+	newButton.title = card.desc;
     newButton.id = "button" + card.number;
     newButton.style =
-      "float:right;border:2px solid black;border-radius:4px;cursor:pointer;padding:3px;background-color:" +
-      color +
-      ";margin:0px 3px";
+      "float:right;border:2px solid black;border-radius:4px;margin:3px;cursor:pointer;font-size: 11px;color: black;padding:3px;background-color:" +
+      color;
     newButton.onclick = function() {
       fillCard(card);
     };
@@ -80,22 +140,10 @@ var initPluginScript = function() {
   var cards = testCreditCards.filter(x => x.type == brand);
 
   var div = document.createElement("div");
-  div.style = "font-size: smaller;padding-bottom: 40px;";
+  div.style = "font-size: smaller;padding-bottom: 70px;";
   div.classList.add("payment-info");
-
-  createButton(div, "invalid card", "f54a4a", cards.filter(x => !x.valid));
-  createButton(
-    div,
-    "valid card",
-    "#67ce67",
-    cards.filter(x => !x.has3dSecure && x.valid)
-  );
-  createButton(
-    div,
-    "valid card /w 3ds",
-    "#28ab28",
-    cards.filter(x => x.has3dSecure && x.valid)
-  );
+  
+  cards.map(c => createButton(div,c));
 
   cvc.parentElement.insertBefore(div, cvc);
 
@@ -107,8 +155,13 @@ var fillCard = function(c) {
   document.getElementById("payment-cardnumber").value = c.number;
   document.getElementById("payment-cardholdername").value = "Plugin " + type;
   document.getElementById("payment-cvc").value = c.cvc;
-  document.getElementById("payment-expirydate-year").selectedIndex = 4;
-  document.getElementById("payment-expirydate-month").selectedIndex = 2;
+  //mask can have a dropdown or an input field for expiry date
+  var yeardd = document.getElementById("payment-expirydate-year");
+  yeardd.selectedIndex = 4;
+  yeardd.value = new Date().getFullYear()+1;
+  var monthdd = document.getElementById("payment-expirydate-month");
+  monthdd.selectedIndex = 2;
+  monthdd.value = new Date().getMonth()+1;
 };
 
 var intervalId = setInterval(initPluginScript, 200);
